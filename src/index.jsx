52b42404001af72556d97888d0dcaf6a71591096
Kevin Molid <kevinmolid@gmail.com>
2024-01-25 18:17:55 +0100

    Trying to fix vite

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+/* Imports */
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+/* Elements */
+import App from "/App"
+
+/* Root */
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+/* Render */
+root.render(
+    <App />
+)
