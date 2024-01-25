abef65544242d58c366658782d060779ff9bac3c
Kevin Molid <kevinmolid@gmail.com>
2024-01-25 16:17:28 +0100

    Added page component

--- index.jsx
+++ index.jsx
@@ -1,5 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+function Page() {
+    return (
+        <main>
+            <h1>HAHA</h1>
+        </main>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
+root.render(
+    <Page />
+)
